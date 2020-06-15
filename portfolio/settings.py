93bcb638da13a916c9e40554fe9a2fda5fb92577
zabhitak <zabhitak1292000@gmail.com>
2020-06-15 23:34:59 +0530

    changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6r0n6y6^x%*#t9zs#j)43oo6b)2p=h2s!ki^d-)s#*_5_ykt5x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['zabhitak-portfolio.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT =os.path.join(BASE_DIR,'static')
+# STATIC_ROOT =os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 
 
