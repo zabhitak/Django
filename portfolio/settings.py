fd295358b68a0987ab07bac062ee597d31c313d6
zabhitak <zabhitak1292000@gmail.com>
2020-06-16 16:12:48 +0530

    changes

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -119,8 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 # STATIC_ROOT =os.path.join(BASE_DIR,'static')
-STATIC_URL = '/static/'
 
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
